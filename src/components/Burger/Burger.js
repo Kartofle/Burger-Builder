129061b3e319a5d36656c445e58039ef682cfbd5
Kartofle <adamrose9058@gmail.com>
2019-02-09 17:49:59 -0800

    added burger component

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -1,7 +1,19 @@
 import React from 'react';
 
-const burger = () => {
-    return ()
+import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
+import styles from './Burger.module.css';
+
+const burger = ( props ) => {
+    return (
+        <div className={styles.Burger}>
+            <BurgerIngredient type='bun-top'/>
+            <BurgerIngredient type='lettuce'/>
+            <BurgerIngredient type='bacon'/>
+            <BurgerIngredient type='cheese'/>
+            <BurgerIngredient type='patty'/>
+            <BurgerIngredient type='bun-bottom'/>
+        </div>
+    );
 };
 
 export default burger;
